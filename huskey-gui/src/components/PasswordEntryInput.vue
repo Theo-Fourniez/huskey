a3a2cfa8056b065b7b22137ed72c674b747a2e31
Theo-Fourniez <10551899+Theo-Fourniez@users.noreply.github.com>
2024-02-07 22:59:36 +0100

    feat(component): added password entry input

---
+++ huskey-gui/src/components/PasswordEntryInput.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { PasswordEntry } from '@/types/huskeyTypes';
+import { PropType, Ref, computed, ref } from "vue";
+
+
+const thisEntry: Ref<PasswordEntry> = ref({
+    name: "",
+    username: "",
+    password: "",
+    url: "",
+});
+
+
+const props = defineProps({
+    entry: {
+        type: Object as PropType<PasswordEntry>,
+        default: undefined
+    }
+});
+
+const hasPassedEntry = computed(() => {
+    return props.entry !== undefined;
+});
+
+const emit = defineEmits<{ (event: 'change', entry: PasswordEntry): void }>();
+
+const iconUrl = computed(() => {
+    return hasPassedEntry.value ? props.entry!.url ? `https://www.google.com/s2/favicons?domain=${props.entry!.url}&sz=32` : undefined : undefined;
+});
+
+if (hasPassedEntry.value) {
+    thisEntry.value = props.entry!;
+}
+
+const buttonClicked = (payload: MouseEvent) => {
+    emit("change", thisEntry.value);
+};
+</script>
+
+<template>
+    <h2>Create an entry</h2>
+    <img :src="iconUrl" class="icon">
+    <input type="text" v-model="thisEntry.name" placeholder="Entry name, website name...">
+    <input type="text" v-model="thisEntry.username" placeholder="Username...">
+    <input type="text" v-model="thisEntry.password" placeholder="Password...">
+    <input type="text" v-model="thisEntry.url" placeholder="Website URL...">
+    <button @click="buttonClicked">Create</button>
+</template>
+
+<style scoped>
+.icon {
+    width: 32px;
+    height: 32px;
+}
+</style>
