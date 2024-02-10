b7d06933c0d1a50c40e496e5b54d96dcd3f0377d
Theo-Fourniez <10551899+Theo-Fourniez@users.noreply.github.com>
2024-02-10 22:50:19 +0100

    dev(back): code cleanup

--- huskey-gui/src-tauri/src/main.rs
+++ huskey-gui/src-tauri/src/main.rs
@@ -5,29 +5,18 @@ mod errors;
 use crate::errors::AppError;
 
 use std::{
-    borrow::BorrowMut,
     ops::Deref,
     path::{Path, PathBuf},
-    sync::{Mutex, MutexGuard},
+    sync::Mutex,
 };
 
-use huskey_lib::{
-    create_or_read_db,
-    database::{Database, PasswordEntry},
-    decrypt_db, encrypt_and_save_db,
-};
+use huskey_lib::{create_or_read_db, database::Database, decrypt_db, encrypt_and_save_db};
 impl Default for OpenedDatabase {
     fn default() -> Self {
         return OpenedDatabase(Mutex::new(None));
     }
 }
 struct OpenedDatabase(Mutex<Option<Database>>);
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 #[tauri::command]
 fn open_database(
@@ -42,19 +31,6 @@ fn open_database(
 
     let decrypted_db = decrypt_db(encrypted_db, password.clone());
 
-    /*     if decrypted_db.is_ok() {
-        println!("is ok");
-        let mut new_db = decrypted_db.unwrap();
-        new_db.add_password(PasswordEntry {
-            name: "My google acc".to_string(),
-            username: "myaccount-52".to_string(),
-            password: "passSecret_*".to_string(),
-            url: Some("https://www.google.com".to_string()),
-        });
-        huskey_lib::encrypt_and_save_db(&new_db, password.clone(), &input_path, None).unwrap();
-        return Ok(new_db);
-    } */
-    //encrypt_and_save_db(&decrypted_db, password, &input_path, None);
     match decrypted_db {
         Ok(db) => {
             let mut d = opened_database.0.lock().unwrap();
@@ -73,23 +49,23 @@ fn save_database(
     path: &str,
     password: String,
     maybe_database: tauri::State<OpenedDatabase>,
-) -> Option<AppError> {
-    let mut mutx = maybe_database.0.lock().unwrap();
+) -> Result<(), AppError> {
+    let mutx = maybe_database.0.lock().unwrap();
     match mutx.deref() {
         Some(db) => {
             let path = Path::new(path);
-            encrypt_and_save_db(db, password, path, Some(db.pbdkf2_rounds)).unwrap();
-            return None;
+            encrypt_and_save_db(db, password, path, Some(db.pbdkf2_rounds))?;
+            return Ok(());
         }
-        None => return Some(AppError::NoDatabaseOpened),
+        None => return Err(AppError::NoDatabaseOpened),
     }
 }
 
 fn main() {
     tauri::Builder::default()
         .manage(OpenedDatabase::default())
-        .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![open_database])
+        .invoke_handler(tauri::generate_handler![save_database])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
