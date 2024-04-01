f6c6ef19bb373c246c3055b3c8c1c2be25fbae8a
Theo-Fourniez <10551899+Theo-Fourniez@users.noreply.github.com>
2024-04-01 15:43:54 +0200

    feat(gui): enhanced password editing / adding

--- huskey-gui/src/components/PasswordEdit.vue
+++ huskey-gui/src/components/PasswordEdit.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, reactive, ref, watch } from "vue";
+import { computed, onMounted, reactive, ref, watch } from "vue";
 import { PasswordEntry } from "@/types/huskeyTypes";
 import { addPasswordEntry, editPasswordEntry } from "@/store/useDatabase";
 import { useRouter } from "vue-router";
@@ -12,7 +12,7 @@ const props = defineProps<{
 
 const isNewEntry = ref(true);
 
-let entry = props.entry;
+let entry = {...props.entry};
 let initialEntry = {...props.entry};
 
 const emit = defineEmits<{edited: [edited: PasswordEntry]}>();
@@ -22,14 +22,11 @@ const emitEdit = () => {
 }
 
 onMounted(() => {
-  console.log("onMounted")
   isNewEntry.value = entry.name === "" && entry.username === "" && entry.password === "" && (entry.url === "" || entry.url === undefined);
 });
 
 const submitForm = async () => {
-  console.log("submitForm")
   emitEdit();
-  console.log("isNewEntry", isNewEntry.value)
   if(isNewEntry.value){
     await addPasswordEntry(entry);
   } else{
@@ -38,27 +35,28 @@ const submitForm = async () => {
   }
 }
 
+const title = computed(() => { return isNewEntry.value ? "Add entry" : "Edit entry"});
+const callToAction = computed(() => { return isNewEntry.value ? "Add" : "Save"});
+
 watch(props, ()=>{
   entry = props.entry;
-  console.log(props);
 });
 </script>
 
 <template>
   <div>
-    <h1>Add a new password</h1>
-    <form style="display: flex; flex-direction: column; justify-content: center; align-items: center; gap: 10px;">
+    <h1>{{ title }}</h1>
+    <form @submit.prevent="submitForm" style="display: flex; flex-direction: column; justify-content: center; align-items: center; gap: 10px;">
       <input type="text" placeholder="Enter name" v-model="entry.name">
       <input type="text" placeholder="Enter username" v-model="entry.username">
       <input type="password" placeholder="Enter a password..." v-model="entry.password">
       <input type="text" placeholder="Enter URL" v-model="entry.url">
-      <button type="submit" @click="submitForm()">Add</button>
+      <button>{{ callToAction }}</button>
     </form>
   </div>
 
 
 </template>
 
-<style>
-
+<style scoped>
 </style>
