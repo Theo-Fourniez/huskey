e5b24e23990f8010409ff908c27bac6a1cb312ca
Theo-Fourniez <10551899+Theo-Fourniez@users.noreply.github.com>
2024-04-15 21:24:12 +0200

    fix(gui): Fixed the behavior and styling

--- huskey-gui/src/components/PasswordEntriesTable.vue
+++ huskey-gui/src/components/PasswordEntriesTable.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { PasswordEntry } from '@/types/huskeyTypes';
-import PasswordEntryRow from './PasswordEntryRow.vue';
-import { useRouter } from 'vue-router';
-import { ref } from 'vue';
 import PasswordEdit from '@/components/PasswordEdit.vue';
+import { PasswordEntry } from '@/types/huskeyTypes';
+import { ref } from 'vue';
+import PasswordEntryRow from './PasswordEntryRow.vue';
 
 
 defineProps<{
@@ -13,20 +12,19 @@ defineProps<{
 const editingEntry = ref<PasswordEntry | null>(null);
 
 const editEntry = (entry: PasswordEntry) => {
-    console.log("clicked ", entry);
-    editingEntry.value = editingEntry.value == entry ? null : entry;
+    editingEntry.value = editingEntry.value == entry ? null : entry; // Close the edit form if the same entry is clicked twice
 }
 
 </script>
 
 <template>
-    <div class="row" style="height: 100vh; overflow-y: scroll;">
-        <div class="column" style="margin: 45px; gap:20px; flex-grow: 1; flex-shrink: 1; overflow-y: scroll; min-height: fit-content;">
+    <div class="row" style="height: 90vh; overflow-y: scroll;">
+        <div class="column" style="margin: 45px; gap:20px; flex-grow: 1; flex-shrink: 1; overflow-y: scroll;">
             <PasswordEntryRow v-if="entries" v-for="entry in entries" :entry="entry" @edit="editEntry($event)"/>
             <div v-else>No entries in this database</div>
         </div>
-        <div style="position: sticky; flex-grow: 2; top:0px;" v-if="editingEntry">
-            <PasswordEdit :entry="editingEntry"></PasswordEdit>
+        <div style="position: sticky; flex-grow: 2; top:0px; overflow-y: scroll;" v-if="editingEntry">
+            <PasswordEdit :entry="editingEntry" @edited="editingEntry = null"></PasswordEdit>
         </div>
     </div>
 </template>
