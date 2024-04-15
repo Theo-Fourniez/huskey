82be414324442084f0a3ac10e043164a157a18d7
Theo-Fourniez <10551899+Theo-Fourniez@users.noreply.github.com>
2024-04-15 21:19:02 +0200

    docs(lib): Added GNUGPLv3 notice

--- lib/lib.rs
+++ lib/lib.rs
@@ -1,3 +1,13 @@
+/*
+This file is part of Huskey.
+
+Huskey is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Huskey is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Huskey. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 //! It is a library that can be used to create, read, update and delete passwords from a database.
 //! The database is encrypted and saved to disk.
 //! Using AES-256-GCM for encryption and PBKDF2 (SHA512) for key derivation.
