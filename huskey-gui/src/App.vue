6e5e2673f232aeadb4021b40b0a899096bc916e6
Theo-Fourniez <10551899+Theo-Fourniez@users.noreply.github.com>
2024-04-17 20:59:20 +0200

    refactor(gui): Removed shortcut poc

--- huskey-gui/src/App.vue
+++ huskey-gui/src/App.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-import { register } from '@tauri-apps/api/globalShortcut';
 import AppNavbar from './components/AppNavbar.vue';
-register("CommandOrControl+S", () => {
-    console.log("Save database shortcut pressed");
-    // saveDatabase(databaseState.cachedPassword);
-});
 
 </script>
 
