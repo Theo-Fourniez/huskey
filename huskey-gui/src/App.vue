246408369fca2f2149db1307e7848b84e0178f9d
Theo-Fourniez <10551899+Theo-Fourniez@users.noreply.github.com>
2024-04-17 21:14:46 +0200

    dev(gui): Added an alert dialog on promise rejected

--- huskey-gui/src/App.vue
+++ huskey-gui/src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import AppNavbar from './components/AppNavbar.vue';
 
+// For debugging purposes
+window.addEventListener('unhandledrejection', function (event) {
+    alert('An error occurred: ' + event.reason);
+    console.log('An error occurred: ', event.reason);
+});
+
 </script>
 
 <template>
